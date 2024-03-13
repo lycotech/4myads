ce3ff8f76f5ba0161b38851d4700c510e394ea77
lycotech <lycotech@gmail.com>
2024-03-13 01:33:35 +0100

    add hero session

--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import classNames from "classnames";
+import { FaFacebook, FaInstagram,  FaShareAlt, FaWhatsapp } from "react-icons/fa"
+import { FaXTwitter } from "react-icons/fa6"
 
 export default function Header() {
   const currentPath = usePathname();
@@ -16,7 +18,7 @@ export default function Header() {
 
 
   return (
-    <nav className="flex space-x-6 justify-between mb-5 items-center py-4 px-5 h-20">
+    <nav className="flex-1 space-x-6 justify-between mb-5 items-center py-4 px-5 h-20 navbar bg-base-100">
      
      {/* Logo Column */}
 
@@ -29,13 +31,13 @@ export default function Header() {
 
 
      {/* Menu Column */}
-     <div className="flex-grow">
-      <ul className="flex space-x-6 justify-center items-center">
+     <div className="flex-grow w-7/12 px-10">
+      <ul className="flex space-x-20 justify-center items-center menu menu-horizontal ">
         {links.map(link => 
         <Link key={link.href} 
         className={classNames({
-          'text-zinc-950':link.href=== currentPath,
-          'text-zinc-500': link.href !== currentPath,
+          'text-green-950':link.href === currentPath,
+          'text-green-500': link.href !== currentPath,
           'hover:text-zinc-800 transition-colors': true
         })} 
         href={link.href}>{link.label}</Link> )}
@@ -46,8 +48,12 @@ export default function Header() {
      </div>
 
      {/* Social Media */}
-     <div className="flex-grow">
-      Social Media
+     <div className="flex-grow space-x-4">
+     <Link href="https://web.facebook.com/4myads/" ><FaFacebook /></Link>
+     <Link href="https://twitter.com/4myads" ><FaXTwitter /></Link>
+     <Link href="https://instagram.com/4myadsOfficial/" ><FaInstagram /></Link>
+     <FaShareAlt />
+     <FaWhatsapp />
      </div>
 
      {/* Add Post*/}
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,12 +1,14 @@
 import { CMS_NAME, CMS_URL } from "../lib/constants";
 
 import Header from "./header";
+import Hero from "./hero";
 
 export default function Intro() {
   return (
     <div>
     
     <Header />
+    <Hero />
 
     </div>
    
